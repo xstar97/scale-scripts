ac0bb9fec416eedb20b82824f22d941cfae428c0
Xstar97TheNoob <9399967+xstar97@users.noreply.github.com>
2023-09-27 18:00:58 -0400

    Update NextAvailableIP.sh

--- scripts/NextAvailableIP.sh
+++ scripts/NextAvailableIP.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 
-# Get the last LoadBalancer IP from k3s kubectl
-last_ip=$(sudo k3s kubectl get svc -A | grep LoadBalancer | awk '{print $5}' | tail -n 1)
+# Get a list of LoadBalancer IPs from k3s kubectl
+ips=$(sudo k3s kubectl get svc -A | grep LoadBalancer | awk '{print $5}')
 
-# Extract the last octet of the IP address
-last_octet=$(echo "$last_ip" | awk -F. '{print $4}')
+# Function to extract the last octet of an IP address
+get_last_octet() {
+    echo "$1" | awk -F. '{print $4}'
+}
 
-# Increment the last octet by 1
-next_octet=$((last_octet + 1))
+# Initialize variables to keep track of the largest IP and its last octet
+largest_ip=""
+largest_octet=0
 
-# Reconstruct the IP address with the incremented octet
-next_ip=$(echo "$last_ip" | awk -F. -v var="$next_octet" '{$4 = var; print}' OFS='.')
+# Iterate through the list of IPs to find the largest one
+for ip in $ips; do
+    octet=$(get_last_octet "$ip")
+    if [ "$octet" -gt "$largest_octet" ]; then
+        largest_ip="$ip"
+        largest_octet="$octet"
+    fi
+done
+
+# Increment the last octet of the largest IP by 1
+next_octet=$((largest_octet + 1))
+
+# Reconstruct the next IP address with the incremented octet
+next_ip=$(echo "$largest_ip" | awk -F. -v var="$next_octet" '{$4 = var; print}' OFS='.')
 
 echo "Next available IP: $next_ip"
