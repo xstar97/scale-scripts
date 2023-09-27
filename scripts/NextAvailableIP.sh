d5ec60275b2106bf39271bafcae645439e69535c
Xstar97TheNoob <9399967+xstar97@users.noreply.github.com>
2023-09-27 19:24:24 -0400

    Update NextAvailableIP.sh

--- scripts/NextAvailableIP.sh
+++ scripts/NextAvailableIP.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 list_apps=false
+test_mode=false
 
 while [[ "$#" -gt 0 ]]; do
   case $1 in
     list-apps)
       list_apps=true
+      shift
+      ;;
+    --test)
+      test_mode=true
       shift
       ;;
     *)
@@ -15,11 +20,20 @@ while [[ "$#" -gt 0 ]]; do
   esac
 done
 
-# Run the command and store the output in a variable
-svc_output=$(sudo k3s kubectl get svc -A)
+if [ "$test_mode" = true ]; then
+  # Example list for testing
+  example_list="traefik-tcp                    10.0.0.178
+fileflows                      10.0.0.174
+plex                           10.0.0.175"
 
-# Extract the list of IPs from the output
-ip_list=$(echo "$svc_output" | awk '$3 == "LoadBalancer" { printf "%-30s %-15s\n", $2, $5 }')
+  ip_list=$(echo "$example_list" | awk '{ printf "%-30s %-15s\n", $1, $2 }')
+else
+  # Run the command and store the output in a variable
+  svc_output=$(sudo k3s kubectl get svc -A)
+
+  # Extract the list of IPs from the output
+  ip_list=$(echo "$svc_output" | awk '$3 == "LoadBalancer" { printf "%-30s %-15s\n", $2, $5 }')
+fi
 
 # Convert the list of IPs to an array
 IFS=$'\n' read -r -a ips <<< "$ip_list"
